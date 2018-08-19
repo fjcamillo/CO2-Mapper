49bc918660727f9a8a4197fb1db174baa56b9caa
fjcamillo <fjcamillo.dev@gmail.com>
2018-08-19 15:08:34 +0800

    deleted generated sqlite db

--- backend/app/routes/carbon.py
+++ backend/app/routes/carbon.py
@@ -4,7 +4,7 @@ from flask_graphql import GraphQLView
 from ..models import carbon as carbonModel
 from ..schemas import carbon as carbonSchema
 
-@app.route('/graphql')
+@app.add_url_rule('/graphql')
 def graph():
     return GraphQLView.as_view(
         'graphql',
