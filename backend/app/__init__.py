297ecd3e3f2fad57dd43a43e551b9f6aed2e6498
fjcamillo <fjcamillo.dev@gmail.com>
2018-08-27 03:22:15 +0800

    testing graphene

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,18 @@
 from flask import Flask
 from dotenv import load_dotenv
 from os import path, environ as env
+from schemas import carbon
+from flask_graphql import GraphQLView
 
 load_dotenv(path.join(path.dirname(__file__), '../.env'))
 
 app = Flask(__name__)
-
-from app.routes import carbon
+app.add_url_rule(
+    '/graphql',
+    view_func=GraphQLView.as_view(
+        'graphql',
+        schema = carbon.schema,
+        graphiql=True
+    )
+)
+# from app.routes import carbon
